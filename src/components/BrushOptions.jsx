4ccc0d6bce26ecaa874e517321780f1c763e6d55
srobalo <s.robalo@campus.fct.unl.pt>
2020-11-22 17:33:17 +0000

    Brush update

--- src/components/BrushOptions.jsx
+++ src/components/BrushOptions.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
+const colors= [
+    "#000000",
+    "#FFFFFF",
+    "#FF0000",
+    "#00FF00",
+    "#0000FF"]
+
 const BrushOptions = ({ value, onChange }) => {
+    //<i class="fas fa-paint-brush"></i>
     return (
         <select name="" id="" value={value} onChange={onChange}>
-            <option value="#E0BBE4" selected>Thistle</option>
-            <option value="#957DAD">Lavender Purple</option>
-            <option value="#D291BC">Pastel Violet</option>
+            <option value={colors[0]} selected>Black</option>
+            <option value={colors[1]}>White</option>
+            <option value={colors[2]}>Red</option>
+            <option value={colors[3]}>Green</option>
+            <option value={colors[4]}>Blue</option>
         </select>
     )
 }
