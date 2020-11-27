893f9cd3bd2200ce3b4f97bf048b7f28fba01ca7
dabadias <daba.dias@campus.fct.unl.pt>
2020-11-27 20:20:12 +0000

    AuthSys menu, base facial features

---
+++ src/views/examples/NewFacialFeatures.js
@@ -0,0 +1,98 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+* Modified by Tactical Design
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Link } from "react-router-dom";
+
+// reactstrap components
+import { Button, Card, Container, Row, Col } from "reactstrap";
+
+// core components
+import DemoNavbar from "components/Navbars/DemoNavbar.js";
+import SimpleFooter from "components/Footers/SimpleFooter.js";
+import AppBase from "components/AppBase";
+
+class NewFacialFeatures extends AppBase {
+
+  state = {
+
+  }
+
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+
+  render() {
+    return (
+      <>
+        <DemoNavbar />
+        <main className="/facial-features" ref="main">
+          <section className="section-profile-cover section-shaped my-0">
+            {/* Circles background */}
+            <div className="shape shape-style-1 shape-default alpha-4">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            {/* SVG separator */}
+            <div className="separator separator-bottom separator-skew">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="none"
+                version="1.1"
+                viewBox="0 0 2560 100"
+                x="0"
+                y="0"
+              >
+                <polygon
+                  className="fill-white"
+                  points="2560 0 2560 100 0 100"
+                />
+              </svg>
+            </div>
+          </section>
+          <section className="section">
+            <Container>
+              <Card className="card-profile shadow mt--300">
+                <div className="px-4">
+                  <div className="text-center mt-5">
+                    <h3>
+                      <i className="fa fa-id-card" />{" "}New facial feature
+                    </h3>
+                  </div>
+                  <div className="mt-5 py-5 border-top text-center">
+                    
+                  </div>
+                </div>
+              </Card>
+            </Container>
+          </section>
+        </main>
+        <SimpleFooter />
+      </>
+    );
+  }
+}
+
+export default NewFacialFeatures;
