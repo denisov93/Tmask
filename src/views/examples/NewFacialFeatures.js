d7df8d2de026cd5d108b29f212a80256aa5dccdc
dabadias <daba.dias@campus.fct.unl.pt>
2020-11-28 02:47:01 +0000

    New facial feature - face shape

--- src/views/examples/NewFacialFeatures.js
+++ src/views/examples/NewFacialFeatures.js
@@ -35,7 +35,27 @@ import {
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
 import AppBase from "components/AppBase";
+
 import doc from "assets/docs/measuring_tool.pdf"
+import oval from "assets/img/faceType/oval.png"
+import square from "assets/img/faceType/square.png"
+import round from "assets/img/faceType/round.png"
+import heart from "assets/img/faceType/heart.png"
+import oblong from "assets/img/faceType/oblong.png"
+import diamond from "assets/img/faceType/diamond.png"
+import Col from "reactstrap/lib/Col";
+
+const rowStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}
+
+const legendStyle = {
+  width: 140,
+  margin: 10,
+  fontSize: 18
+}
 
 function BottomButton(props) {
   return (
@@ -49,10 +69,12 @@ function BottomButton(props) {
     </Button>
   );
 }
+
 class NewFacialFeatures extends AppBase {
 
   state = {
-    index: 1,
+    index: 0,
+    face: "",
     xaxis: 0,
     yaxis: 0,
   }
@@ -63,50 +85,101 @@ class NewFacialFeatures extends AppBase {
     this.refs.main.scrollTop = 0;
   }
 
+  faceButton(img, i) {
+    let isSelected = i === this.state.face ? "double" : "none"
+
+    return (
+      <Button
+        color="primary"
+        style={{ background: "white", borderStyle: isSelected, }}
+        onClick={() => this.setState({ face: i })}>
+        <img src={img} alt="" />
+      </Button>
+    );
+  }
+
   renderSwitch() {
     switch (this.state.index) {
       case 0:
-        return this.state.index;
+        return (
+          <div>
+            <h4 className="py-5">Face Shape</h4>
+            <Col>
+              <Row style={rowStyle}>
+                {this.faceButton(oval, "oval")}
+                {this.faceButton(square, "square")}
+                {this.faceButton(round, "round")}
+              </Row>
+              <Row style={rowStyle}>
+                <p style={legendStyle}>Oval</p>
+                <p style={legendStyle}>Square</p>
+                <p style={legendStyle}>Round</p>
+              </Row>
+              <Row style={rowStyle}>
+                {this.faceButton(heart, "heart")}
+                {this.faceButton(oblong, "oblong")}
+                {this.faceButton(diamond, "diamond")}
+              </Row>
+              <Row style={rowStyle}>
+                <p style={legendStyle}>Heart</p>
+                <p style={legendStyle}>Oblong</p>
+                <p style={legendStyle}>Diamond</p>
+              </Row>
+            </Col>
+          </div>
+        )
       case 1:
         return (
           <div>
-            <h6 className="margin-b">Distance (in cm)</h6>
-            <Row className="row-cols-3 margin-b" style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-              <FormGroup className="mb-3 margin-r">
+            <h4 className="py-5">Distances</h4>
+            <Col>
+              <Row style={rowStyle} className="row-cols-3">
+                <p style={{ marginRight: 16, marginBottom: 10, }}>From the nose bridge to under the chin</p>
+                <p style={{ marginLeft: 16, marginBottom: 10, }}>Between the ears across the chin</p>
+              </Row>
+              <Row style={rowStyle} className="row-cols-3 margin-b">
+                <FormGroup className="mb-3 margin-r">
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="fa fa-arrows-v" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input id="id_xaxis"
+                      type="number" value={this.state.xaxis} min="1" max="25" step="0.25"
+                      onChange={e => this.setState({ xaxis: e.target.value })} />
+                    <InputGroupAddon addonType="append">
+                      <InputGroupText>centimeters</InputGroupText>
+                    </InputGroupAddon>
+                  </InputGroup>
+                </FormGroup>
+                <FormGroup className="mb-3 margin-l">
                 <InputGroup className="input-group-alternative">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="fa fa-arrows-v" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input id="id_account" placeholder="From the nose bridge to under the chin" type="number" min="1" max="25" step="0.25" onChange={e => this.setState({ xaxis: e.target.value })} />
-                </InputGroup>
-              </FormGroup>
-              <FormGroup className="mb-3 margin-l">
-                <InputGroup className="input-group-alternative">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="fa fa-arrows-h" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input id="id_account" placeholder="Between the ears across the chin" type="number" min="1" max="50" step="0.25" onChange={e => this.setState({ yaxis: e.target.value })} />
-                </InputGroup>
-              </FormGroup>
-            </Row>
-            <Button
-              className="btn-neutral btn-icon btn-std-case"
-              color="default">
-              <span className="btn-inner--icon">
-                <i className="fa fa-file-pdf-o mr-2" />
-              </span>
-              <span className="nav-link-inner--text ml-1">
-                <a href = {doc} target = "_blank" rel="noreferrer" >Get out measurement tool!</a>
-              </span>
-            </Button>
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="fa fa-arrows-h" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input id="id_yaxis" placeholder="Between the ears across the chin"
+                      type="number" value={this.state.yaxis} min="1" max="50" step="0.25"
+                      onChange={e => this.setState({ yaxis: e.target.value })} />
+                    <InputGroupAddon addonType="append">
+                      <InputGroupText>centimeters</InputGroupText>
+                    </InputGroupAddon>
+                  </InputGroup>
+                </FormGroup>
+              </Row>
+              <Button
+                className="btn-neutral btn-icon btn-std-case"
+                color="default">
+                <span className="btn-inner--icon">
+                  <i className="fa fa-file-pdf-o mr-2" />
+                </span>
+                <span className="nav-link-inner--text ml-1">
+                  <a href={doc} target="_blank" rel="noreferrer" >Get out measurement tool!</a>
+                </span>
+              </Button>
+            </Col>
           </div>
         );
       default:
@@ -148,6 +221,7 @@ class NewFacialFeatures extends AppBase {
   }
 
   render() {
+    console.log("xaxis", this.state.xaxis)
     return (
       <>
         <DemoNavbar />
@@ -182,23 +256,17 @@ class NewFacialFeatures extends AppBase {
           </section>
           <section className="section">
             <Container>
-              <Card className="card-profile shadow mt--300">
+              <Card className="card-profile shadow mt--450">
                 <div className="px-4">
                   <div className="text-center mt-5">
                     <h3>
                       <i className="fa fa-id-card" />{" "}New facial feature
                     </h3>
                   </div>
-                  <div className="mt-5 py-5 border-top text-center">
+                  <div className="mt-5 border-top text-center">
                     <Form role="form" onSubmit={this.handleSubmit}>
                       {this.renderSwitch()}
-                      <Row style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                      }}>
-                        {this.renderBottomButtons()}
-                      </Row>
+                      {this.renderBottomButtons()}
                     </Form>
                   </div>
                 </div>
