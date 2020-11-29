db4a7cc23634458a383e7070311ddb72fdeb8a9b
srobalo <s.robalo@campus.fct.unl.pt>
2020-11-29 01:32:11 +0000

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
+const date = new Date().getTime();
 
 gulp.task("licenses", async function() {
   // this is to add Creative Tim licenses in the production mode for the minified js
@@ -12,13 +13,12 @@ gulp.task("licenses", async function() {
 * Argon Design System React - v1.1.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
 * Copyright 2020 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
 * Modified by Tactical Design
-
+`(date)`
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -37,13 +37,12 @@ gulp.task("licenses", async function() {
 * Argon Design System React - v1.1.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
 * Copyright 2020 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
 * Modified by Tactical Design
-
+`(date)`
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -62,13 +61,12 @@ gulp.task("licenses", async function() {
 * Argon Design System React - v1.1.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
 * Copyright 2020 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
 
 * Coded by Creative Tim
 * Modified by Tactical Design
-
+`(date)`
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
